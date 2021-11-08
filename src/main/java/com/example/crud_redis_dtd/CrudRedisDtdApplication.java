008684f46b13a2384957fe04d735b2f75cd43f45
diemmoon <duongdiem20@gmail.com>
2021-11-08 10:24:51 +0700

    first commit

---
+++ src/main/java/com/example/crud_redis_dtd/CrudRedisDtdApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_dtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRedisDtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRedisDtdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud_redis_dtd/CrudRedisDtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_dtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisDtdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
