c3911151d466930b6ae034c10956a04dae7ff6be
diemmoon <duongdiem20@gmail.com>
2021-11-08 16:08:39 +0700

    set

--- src/main/java/com/example/crud_redis_dtd/repository/EmployeeRepository.java
+++ src/main/java/com/example/crud_redis_dtd/repository/EmployeeRepository.java
@@ -5,15 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class EmployeeRepository {
 
     private HashOperations hashOperations;//crud hash
     private ListOperations listOperations;
+    private SetOperations setOperations;
     private String LIST_KEY = "EMPLOYEE-LIST";
     private String SET_KEY = "EMPLOYEE-SET";
 
@@ -24,6 +27,7 @@ public class EmployeeRepository {
 
         this.hashOperations = redisTemplate.opsForHash();
         this.listOperations= redisTemplate.opsForList();
+        this.setOperations= redisTemplate.opsForSet();
         this.redisTemplate = redisTemplate;
 
     }
@@ -31,26 +35,29 @@ public class EmployeeRepository {
     public void saveEmployee(Employee employee){
 
 //        hashOperations.put("EMPLOYEE", employee.getId(), employee);
-        listOperations.leftPush("LIST_KEY", employee) ;
+//        listOperations.leftPush("LIST_KEY", employee) ;
+        setOperations.add("SET_KEY",employee);
     }
 
-    public List<Employee> findAll(){
+    public Set<Employee> findAll(){
 
 //        return hashOperations.values("EMPLOYEE");
 
-        Long lastIndex = listOperations.size("LIST_KEY") - 1 ;
-        System.out.println("last index:"+ listOperations.size("list-employee"));
-        System.out.println("list:"+ listOperations.range("list-employee", 0, lastIndex));
+//        Long lastIndex = listOperations.size("LIST_KEY") - 1 ;
+//
+//        return listOperations.range("LIST_KEY", 0, lastIndex);
 
-        return listOperations.range("LIST_KEY", 0, lastIndex);
-
+        return setOperations.members("SET_KEY");
     }
     public Employee findById(Integer id){
 
 //        return (Employee) hashOperations.get("EMPLOYEE", id);
 //        crud list
 
-        List<Employee> employees = findAll();
+//        List<Employee> employees = findAll();
+
+        Set<Employee> employees = findAll();
+
         for (Employee employee : employees) {
             if (employee.getId() == id)
                 return employee;
@@ -66,6 +73,9 @@ public class EmployeeRepository {
 //        hashOperations.delete("EMPLOYEE", id);
 
 //        crud list
-        listOperations.rightPopAndLeftPush("LIST_KEY", id);
+//        listOperations.rightPopAndLeftPush("LIST_KEY", id);
+
+        setOperations.remove("SET_KEY", findById(id));
+
     }
 }
